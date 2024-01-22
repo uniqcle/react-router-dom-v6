7b1ede1468a88f7caae2a7965e3a323841599ba5
Andrey <uniqcle@yandex.ru>
2024-01-22 15:57:15 +0700

    added pages to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 
 function App() {
   return (
     <>
       <header>
         <a href="/">home</a>
-        <a href="/blog">home</a>
-        <a href="/about">home</a>
+        <a href="/blog">blog</a>
+        <a href="/about">about</a>
       </header>
       <div>
         <h2>get started...</h2>
