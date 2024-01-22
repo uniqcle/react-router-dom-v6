15a06374a87b5708f2d9383b667ac02d30472b5c
Andrey <uniqcle@yandex.ru>
2024-01-22 15:53:08 +0700

    init pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <header>
+        <a href="/">home</a>
+        <a href="/blog">home</a>
+        <a href="/about">home</a>
+      </header>
+      <div>
+        <h2>get started...</h2>
+      </div>
+    </>
+  );
 }
 
 export default App;
