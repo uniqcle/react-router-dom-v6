3dc0e6456964069959cc56e0fbad7730fa236e94
Andrey <uniqcle@yandex.ru>
2024-01-24 11:18:01 +0700

    search params in url

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+
+import BlogFilter from "../components/BlogFilter";
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
-  console.log(useLocation());
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get("post") || "";
+  const latest = searchParams.get("latest");
+
+  const startsFrom = latest ? 80 : 1;
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -14,15 +21,24 @@ const Blog = () => {
   return (
     <div className="mt-5 mb-5">
       <h1>Blog...</h1>
+      <BlogFilter
+        postQuery={postQuery}
+        latest={latest}
+        setSearchParams={setSearchParams}
+      />
       <Link to="/blog/new">Add new post</Link>
       <div>
-        {posts.map((post) => {
-          return (
-            <div key={post.id}>
-              {post.id} <Link to={`/blog/${post.id}`}>{post.title}</Link>
-            </div>
-          );
-        })}
+        {posts
+          .filter(
+            (post) => post.title.includes(postQuery) && post.id >= startsFrom
+          )
+          .map((post) => {
+            return (
+              <div key={post.id}>
+                {post.id} <Link to={`/blog/${post.id}`}>{post.title}</Link>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
